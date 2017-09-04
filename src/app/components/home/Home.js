dfdb6c8b8445ea289ba71da180fbe6509bba7756
Daniel Toro <danieltoro@iMac-de-Daniel.local>
2017-09-04 03:19:00 -0300

    Basic Configurations

---
+++ src/app/components/home/Home.js
@@ -0,0 +1,17 @@
+// Dependencies
+import React, { Component } from 'react';
+
+// Styles
+import styles from './Home.scss';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className={styles.home}>
+        Bienvenidos a Feria Inmobiliaria
+      </div>
+    );
+  }
+}
+
+export default Home;
